4e1ec18a9df354d4569773ccb33110cfa77b9631
merawi <merawiyohannes@gmail.com>
2025-06-12 10:49:46 -0700

    add projects page

--- adress/urls.py
+++ adress/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("adress/", views.adress_view, name='adress_view')
+    path("adress/", views.adress_view, name='adress_view'),
+    path("myprojects/", views.myprojects_view, name='myprojects_view')
 ]
--- adress/views.py
+++ adress/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def adress_view(request):
     return render(request, 'adress/adress.html')
+
+def myprojects_view(request):
+    return render(request, 'adress/projects.html')
